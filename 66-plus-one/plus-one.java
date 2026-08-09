5fdcfdaa02d5d673f46e0625bd3cfccbd339bf84
BITU SAHU <sahubitu353@gmail.com>
2026-08-09 15:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (80.20%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        // Right se left traverse
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            // Agar digit 9 nahi hai
+            if (digits[i] < 9) {
+                digits[i]+=1;
+                return digits;
+            }
+
+            // Agar digit 9 hai
+            // 9 + 1 = 10
+            digits[i] = 0;
+        }
+
+        // Agar yahan aaye,
+        // iska matlab saare digits 9 the
+
+        int[] result = new int[digits.length + 1];
+
+        result[0] = 1;
+
+        return result;
+    }
+}
